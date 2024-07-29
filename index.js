ceb4494539ced5e96359337b8a7476219e86240f
Scott <ythtppmm@gmail.com>
2024-07-30 07:48:34 +0900

    build

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("Asia/Kolkata");
+    currentTime.setTimezone("Asia/Tokyo");
     callback(null, {
         statusCode: '200',
-        body: 'The time in India is: ' + currentTime.toString(),
+        body: 'The time in Japan is: ' + currentTime.toString(),
     });
 };

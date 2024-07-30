e154589430082f8d4e187a0a7b8f5f6781fd35e6
Scott <ythtppmm@gmail.com>
2024-07-30 11:29:38 +0900

    build

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var time = require('time');
+const dateAndTime = require('date-and-time');
+
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("Asia/Tokyo");
+    // Set the timezone to Tokyo
+    dateAndTime.timezone('Asia/Tokyo');
+
+    // Get the current time
+    const currentTime = dateAndTime.format(new Date(), 'YYYY/MM/DD HH:mm:ss');
+    
     callback(null, {
         statusCode: '200',
         body: 'The time in Japan is: ' + currentTime.toString(),

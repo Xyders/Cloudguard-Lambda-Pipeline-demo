3aef9419e89997797be5a60922494bdf7d61156f
Scott <ythtppmm@gmail.com>
2024-07-30 22:23:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 const moment = require('moment-timezone');
 
 exports.handler = function (event, context, callback) {
-    const todayMoment = moment().tz('Asia/Tokyo');
+    const todayMoment = moment().tz('Asia/Tokyo').format();
 
     callback(null, {
         statusCode: '200',
-        body: 'The time in India is: ' + todayMoment,
+        body: 'The time in Japan is: ' + todayMoment,
     });
-    
 }
